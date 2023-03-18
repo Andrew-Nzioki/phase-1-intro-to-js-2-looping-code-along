1344da78663ccfaeb6880d7e49c4ab9e16e9f0f2
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-03-17 23:51:40 -0500

    add wrap gift function

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // Code your solutions in this file
-console.log('looping lab')
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  debugger;
+}
+
+function writeCards(names, eventName) {
+  for (i = 0; i < names.length; i++) {}
+}
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (i = 0; i < gifts.length; i++) {}
+}
